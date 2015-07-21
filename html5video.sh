86396a82ffe5318216ca1552928746bba6787375
sean <seanhodges84@gmail.com>
2015-07-21 20:56:23 +0100

    Added the HTML5 tests

--- html5video.sh
+++ html5video.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-phantomjs ./phantomtest.js
+phantomjs ./phantomrecorder.js
 rm out.mp4
 ffmpeg -framerate 10 -i temp/output%02d.png -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
 rm temp/output*.png
