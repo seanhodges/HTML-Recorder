10c086f15158378c74af36461caa9f04ce819888
sean <shodges@wav2.com>
2016-02-24 17:50:48 +0000

    Updated the tests

--- test/convertSpec.js
+++ test/convertSpec.js
@@ -8,18 +8,20 @@ var expect = chai.expect;
 
 describe('html edge poster generation', function() {
 
-    it('should generate a poster for MNN487118_Test.zip via a GET', function (done) {
-        superagent
-            .get('http://localhost:17142/convert/test.png?src=http://localhost/MNN487118_Test/Entertainment1+300+x+250.html&width=300&height=250')
-            .end(function (err, res) {
-                expect(res.status).to.equal(200);
-                done();
-            })
-    });
+    var TARGET_HOST = 'http://localhost:17142';
+
+//    it('should generate a poster for MNN487118_Test.zip via a GET', function (done) {
+//        superagent
+//            .get(TARGET_HOST + '/convert/test.png?src=http://localhost/MNN487118_Test/Entertainment1+300+x+250.html&width=300&height=250')
+//            .end(function (err, res) {
+//                expect(res.status).to.equal(200);
+//                done();
+//            })
+//    });
 
     it('should generate a CC2015 poster for MNN487118_Test.zip via a POST', function (done) {
         superagent
-            .post('http://localhost:17142/convert/test.png')
+            .post(TARGET_HOST + '/convert/test.png')
             .type('form')
             .attach('creative', 'test-resources/MNN487118_Test.zip', 'MNN487118_Test.zip')
             .end(function (err, res) {
@@ -28,13 +30,13 @@ describe('html edge poster generation', function() {
             })
     });
 
-    it('should generate a CC2015 poster for test-fitting-test.zip via a POST', function (done) {
+    it('should gracefully fail when attempting to create poster for test-fitting-test.zip via a POST', function (done) {
         superagent
-            .post('http://localhost:17142/convert/test.png')
+            .post(TARGET_HOST + '/convert/test.png')
             .type('form')
             .attach('creative', 'test-resources/test-fitting-test.zip', 'test-fitting-test.zip')
             .end(function (err, res) {
-                expect(res.status).to.equal(200);
+                expect(res.status).to.equal(500);
                 done();
             })
     });
